a3167f157d7e8dd95ca76bdabe7b2b4534a7828e
Mirum-cor <laetitia-caritas@mail.ru>
2021-04-11 19:12:04 +0300

    feat: partly solved 10-find-index (1 of 5 tests is failed)

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,21 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let res = -1;
+  /* array.find((num, index) => {
+    if (num === value) {
+      res = index;
+      return res;
+    }
+  }); */
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      res = i;
+      break;
+    }
+  }
+  return res;
 }
 
 module.exports = findIndex;
