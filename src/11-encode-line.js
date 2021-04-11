93f66aac3504e5dfbc0c5b937e576ad2d07052b6
Mirum-cor <laetitia-caritas@mail.ru>
2021-04-11 15:15:19 +0300

    feat: solve 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const strArr = str.split('');
+  let counter = 1;
+  let res = '';
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i + 1] && strArr[i + 1] === strArr[i]) {
+      counter++;
+    } else if (counter > 1) {
+      res += `${counter}${strArr[i]}`;
+      counter = 1;
+    } else {
+      res += strArr[i];
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
