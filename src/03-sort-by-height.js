42615c9e980c552fbca3377795ad8546173f1c41
Mirum-cor <laetitia-caritas@mail.ru>
2021-04-11 14:36:23 +0300

    feat: solve 03-sort-by-height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,18 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const res = arr;
+  for (let i = 0; i < res.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (res[j] > res[i] && res[i] !== -1 && res[j] !== -1) {
+        const boofer = res[i];
+        res[i] = res[j];
+        res[j] = boofer;
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = sortByHeight;
