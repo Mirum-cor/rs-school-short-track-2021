06f14a0d5e919dd6aece5225ad5f92198c91cfa3
Mirum-cor <laetitia-caritas@mail.ru>
2021-04-11 14:30:11 +0300

    feat: solve 06-mac-address

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,12 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const regexp = /[0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]-[0-9A-F][0-9A-F]/g;
+  if (n.match(regexp)) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isMAC48Address;
