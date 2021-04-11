056e4e4f51d4349b5b974470e5fce3be4482b5d3
Mirum-cor <laetitia-caritas@mail.ru>
2021-04-11 16:36:53 +0300

    feat: solve 05-delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,24 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const nArr = String(n).split('');
+  let max = n % 10;
+  for (let i = 0; i < nArr.length; i++) {
+    // const digit = String(n % (10 ** (i + 1))).slice(0, 1);
+    const arrWithoutDigit = [];
+    // const nWithoutDigit = +nArr.filter((member) => member !== digit).join('');
+    for (let j = 0; j < nArr.length; j++) {
+      if (i !== j) {
+        arrWithoutDigit.push(nArr[j]);
+      }
+    }
+    const nWithoutDigit = +arrWithoutDigit.join('');
+    if (nWithoutDigit > max) {
+      max = nWithoutDigit;
+    }
+  }
+  return max;
 }
 
 module.exports = deleteDigit;
